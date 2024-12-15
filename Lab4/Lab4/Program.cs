fa9b484f7050a508bab29f0b93766067a53d5d17
Kamil Pawlak <hello@kamilpawlak.com>
2024-12-15 10:18:10 +0100

    Lab5 init

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -1,13 +1,14 @@
 ﻿// Zadanie 1
 
-using Lab4;
+//using Lab4;
 
-List<Shape> shapes = new List<Shape>();
-shapes.Add(new Rectangle(10, 20, 30, 40));
-shapes.Add(new Triangle(15, 25, 35, 55));
-shapes.Add(new Circle(15, 55, 10));
+//List<Shape> shapes = new List<Shape>();
+//shapes.Add(new Rectangle(10, 20, 30, 40));
+//shapes.Add(new Triangle(15, 25, 35, 55));
+//shapes.Add(new Circle(15, 55, 10));
 
-foreach (Shape shape in shapes)
-{
-    shape.Draw();
-}
+//foreach (Shape shape in shapes)
+//{
+//    shape.Draw();
+//}
+
---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,81 @@
+﻿
+//enum Kolor
+//{
+//    Czerwony, //0
+//    Zielony, //1
+//    Żółty, //2
+//    Niebieski = 10,
+//    Czarny //11
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Kolor mojKolor = Kolor.Czerwony;
+//        Console.WriteLine($"Wybrany kolor: {mojKolor}");
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        try
+//        {
+//            Console.WriteLine("Podaj licznik:");
+//            int licznik = int.Parse(Console.ReadLine());
+//            Console.WriteLine("Podaj mianownik:");
+//            int mianownik = int.Parse(Console.ReadLine());
+
+//            int wynik = licznik / mianownik;
+//            Console.WriteLine($"Wynik dzielenia: {wynik}");
+//        }
+//        catch (DivideByZeroException ex)
+//        {
+//            Console.WriteLine("Nie można dzielić przez zero");
+//            Console.WriteLine(ex.Message);
+//        }
+//        catch (FormatException ex)
+//        {
+//            Console.WriteLine("Nieprawidłowy format danych");
+//            Console.WriteLine(ex.Message);
+//        }
+//        finally
+//        {
+//            Console.WriteLine("Wykonał się blok finally");
+//        }
+//    }
+//}
+
+class MojeWyjatki : Exception
+{
+    public MojeWyjatki(string message) : base(message)
+    {
+
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            SprawdzLiczbe(-23);
+        }
+        catch (MojeWyjatki e)
+        {
+            { Console.WriteLine(e.Message); }
+        }
+
+        static void SprawdzLiczbe(int liczba)
+        {
+            if (liczba <= 0)
+            {
+                throw new MojeWyjatki("Liczba musi byc wieksza od zera!");
+            }
+        }
+    }
+}
