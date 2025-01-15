6eba78fa65dd9daa53894cad1c7aa82226f19678
Kamil Pawlak <hello@kamilpawlak.com>
2025-01-15 21:38:13 +0100

    Lab5 task 3

--- Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
 // zadanie 1
 
 //enum Operation
@@ -161,109 +163,163 @@
 
 // zadanie 2
 
-enum OrderStatus
-{
-    Oczekujące = 1,
-    Przyjęte,
-    Zrealizowane,
-    Anulowane
-}
+//enum OrderStatus
+//{
+//    Oczekujące = 1,
+//    Przyjęte,
+//    Zrealizowane,
+//    Anulowane
+//}
 
-enum Operation
+//enum Operation
+//{
+//    View = 1,
+//    Change,
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Dictionary<int, List<string>> orderContents = new Dictionary<int, List<string>>() {
+//            {1, ["masło", "mleko"] },
+//            {2, ["chleb", "szynka"] }
+//        };
+
+//        Dictionary<int, OrderStatus> orderStatuses = new Dictionary<int, OrderStatus>() {
+//            {1, OrderStatus.Przyjęte },
+//            {2, OrderStatus.Zrealizowane }
+//        };
+
+//        Operation? selection = null;
+
+//        do
+//        {
+//            do
+//            {
+//                Console.WriteLine("Podaj typ operacji:");
+//                Console.WriteLine("1- lista zamówień:");
+//                Console.WriteLine("2- zmiana statusu");
+//                Console.WriteLine("0- zakończ");
+
+//                try
+//                {
+//                    int input = int.Parse(Console.ReadLine());
+//                    if (input == 0) return;
+//                    if (Enum.IsDefined(typeof(Operation), input)) selection = (Operation)input;
+//                    else Console.WriteLine("Nieprawidłowy wybór");
+//                }
+//                catch (FormatException ex) { Console.WriteLine("Nieprawidłowy format danych"); }
+//                catch (Exception ex) { Console.WriteLine(ex.Message); };
+//            } while (selection == null);
+
+//            if (selection == Operation.View)
+//            {
+//                foreach (var item in orderStatuses)
+//                {
+//                    Console.WriteLine($"Zamówienie {item.Key}: ");
+//                    Console.WriteLine($"Status {item.Value}");
+//                    Console.WriteLine($"Zawartość {String.Join(",", orderContents[item.Key])}");
+//                }
+//            }
+
+//            else if (selection == Operation.Change)
+//            {
+//                int? orderId = null;
+//                do
+//                {
+//                    try
+//                    {
+//                        Console.WriteLine("Podaj id zamówienia:");
+//                        int input = int.Parse(Console.ReadLine());
+//                        if (orderStatuses.ContainsKey(input)) orderId = input;
+//                        else throw new KeyNotFoundException();
+//                    }
+//                    catch (KeyNotFoundException ex) { Console.WriteLine("Nie znaleziono takiego zamówienia"); }
+//                    catch (FormatException ex) { Console.WriteLine("Nieprawidłowy format danych"); }
+//                    catch (Exception ex) { Console.WriteLine(ex.Message); };
+//                } while (orderId == null);
+
+//                OrderStatus? newStatus = null;
+//                do
+//                {
+//                    try
+//                    {
+//                        Console.WriteLine("Podaj nowy status:");
+//                        Console.WriteLine("1- Oczekujące");
+//                        Console.WriteLine("2- Przyjęte");
+//                        Console.WriteLine("3- Zrealizowane");
+//                        Console.WriteLine("4- Anulowane");
+//                        int input = int.Parse(Console.ReadLine());
+//                        if (Enum.IsDefined(typeof(OrderStatus), input))
+//                        {
+//                            if ((OrderStatus)input != orderStatuses[(int)orderId]) newStatus = (OrderStatus)input;
+//                            else throw new ArgumentException();
+//                        }
+//                        else Console.WriteLine("Nieprawidłowy wybór");
+//                    }
+//                    catch (ArgumentException ex) { Console.WriteLine("Status nie może być taki sam"); }
+//                    catch (FormatException ex) { Console.WriteLine("Nieprawidłowy format danych"); }
+//                    catch (Exception ex) { Console.WriteLine(ex.Message); };
+//                } while (newStatus == null);
+//                if (newStatus != null && orderId != null) orderStatuses[(int)orderId] = (OrderStatus)newStatus;
+//            }
+
+//            selection = null;
+//        } while (true);
+//    }
+//}
+
+// zadanie 3
+
+enum Color
 {
-    View = 1,
-    Change,
+    Czerwony,
+    Niebieski,
+    Zielony,
+    Żółty,
+    Fioletowy
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        Dictionary<int, List<string>> orderContents = new Dictionary<int, List<string>>() {
-            {1, ["masło", "mleko"] },
-            {2, ["chleb", "szynka"] }
-        };
-
-        Dictionary<int, OrderStatus> orderStatuses = new Dictionary<int, OrderStatus>() {
-            {1, OrderStatus.Przyjęte },
-            {2, OrderStatus.Zrealizowane }
-        };
-
-        Operation? selection = null;
+        List<Color> availableColors = new List<Color>([
+            Color.Czerwony,
+            Color.Niebieski,
+            Color.Zielony,
+            Color.Fioletowy,
+            Color.Żółty
+            ]);
+        Random r = new Random();
+        int randomNumber = r.Next(0, availableColors.Count);
+        Color randomColor = availableColors[randomNumber];
+        Color? guessedColor = null;
 
         do
         {
-            do
+            try
             {
-                Console.WriteLine("Podaj typ operacji:");
-                Console.WriteLine("1- lista zamówień:");
-                Console.WriteLine("2- zmiana statusu");
-                Console.WriteLine("0- zakończ");
-
-                try
+                Console.WriteLine("Podaj kolor:");
+                Console.WriteLine(String.Join(",", availableColors));
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out _))
                 {
-                    int input = int.Parse(Console.ReadLine());
-                    if (input == 0) return;
-                    if (Enum.IsDefined(typeof(Operation), input)) selection = (Operation)input;
-                    else Console.WriteLine("Nieprawidłowy wybór");
+                    throw new ArgumentException();
                 }
-                catch (FormatException ex) { Console.WriteLine("Nieprawidłowy format danych"); }
-                catch (Exception ex) { Console.WriteLine(ex.Message); };
-            } while (selection == null);
-
-            if (selection == Operation.View)
-            {
-                foreach (var item in orderStatuses)
+                if (Enum.TryParse(input, out Color selectedColor))
                 {
-                    Console.WriteLine($"Zamówienie {item.Key}: ");
-                    Console.WriteLine($"Status {item.Value}");
-                    Console.WriteLine($"Zawartość {String.Join(",", orderContents[item.Key])}");
+                    if (selectedColor == randomColor) guessedColor = selectedColor;
+                    else Console.WriteLine("zły wybór");
                 }
+                else throw new ArgumentException();
             }
+            catch (ArgumentException ex) { Console.WriteLine("Nieprawidłowy kolor"); }
+            catch (FormatException ex) { Console.WriteLine("Nieprawidłowy format danych"); }
+            catch (Exception ex) { Console.WriteLine(ex.Message); };
+        } while (guessedColor == null);
 
-            else if (selection == Operation.Change)
-            {
-                int? orderId = null;
-                do
-                {
-                    try
-                    {
-                        Console.WriteLine("Podaj id zamówienia:");
-                        int input = int.Parse(Console.ReadLine());
-                        if (orderStatuses.ContainsKey(input)) orderId = input;
-                        else throw new KeyNotFoundException();
-                    }
-                    catch (KeyNotFoundException ex) { Console.WriteLine("Nie znaleziono takiego zamówienia"); }
-                    catch (FormatException ex) { Console.WriteLine("Nieprawidłowy format danych"); }
-                    catch (Exception ex) { Console.WriteLine(ex.Message); };
-                } while (orderId == null);
-
-                OrderStatus? newStatus = null;
-                do
-                {
-                    try
-                    {
-                        Console.WriteLine("Podaj nowy status:");
-                        Console.WriteLine("1- Oczekujące");
-                        Console.WriteLine("2- Przyjęte");
-                        Console.WriteLine("3- Zrealizowane");
-                        Console.WriteLine("4- Anulowane");
-                        int input = int.Parse(Console.ReadLine());
-                        if (Enum.IsDefined(typeof(OrderStatus), input))
-                        {
-                            if ((OrderStatus)input != orderStatuses[(int)orderId]) newStatus = (OrderStatus)input;
-                            else throw new ArgumentException();
-                        }
-                        else Console.WriteLine("Nieprawidłowy wybór");
-                    }
-                    catch (ArgumentException ex) { Console.WriteLine("Status nie może być taki sam"); }
-                    catch (FormatException ex) { Console.WriteLine("Nieprawidłowy format danych"); }
-                    catch (Exception ex) { Console.WriteLine(ex.Message); };
-                } while (newStatus == null);
-                if (newStatus != null && orderId != null) orderStatuses[(int)orderId] = (OrderStatus)newStatus;
-            }
-
-            selection = null;
-        } while (true);
+        Console.WriteLine("Udało się!");
     }
 }
