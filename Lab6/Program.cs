2c7763954701be16108ab299b66195fd2bd589eb
Kamil Pawlak <hello@kamilpawlak.com>
2025-01-19 09:21:59 +0100

    Lab6 init

---
+++ Lab6/Program.cs
@@ -0,0 +1,13 @@
+﻿// zadanie 1
+
+string? fileName;
+do
+{
+    Console.WriteLine("podaj nazwe pliku"); fileName = Console.ReadLine();
+
+} while (fileName == null);
+
+using (StreamWriter sw = new StreamWriter(fileName))
+{
+    sw.WriteLine("w69831");
+}
